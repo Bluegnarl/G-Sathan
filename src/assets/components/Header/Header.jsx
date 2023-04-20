113e89eea050d4b2f8a2ddf3c938823e2a7af61e
Bluegnarl <dradarkx@gmail.com>
2023-04-20 20:41:21 +0200

    Header + About are working correctly

--- src/assets/components/Header/Header.jsx
+++ src/assets/components/Header/Header.jsx
@@ -1,16 +1,37 @@
-import backgroundimage from '../../images/dqsdqsd.png'
-import About from '../About/About'
+import backgroundimage from "../../images/backgroundimage.png";
+import test from "../../images/test.png";
+import About from "../About/About";
 
 export default function Header({ appSize }) {
-  return(
-    <div className={`${appSize < 950 ? 'd-flex-column-reverse' : 'd-flex-row'} w-full jc-space-between`} style={{ height: appSize < 950 ? 'auto' : '40%'}}>
-      <About appSize={appSize} />
-      <img 
-        className='b-cover'
-        style={{ width: appSize < 950 ? '100%' : 'auto', height: appSize > 950 && '100%' }}
+  return (
+    <div
+      className={`${
+        appSize >= 950 ? "d-flex-row" : "d-flex-column-reverse"
+      } w-full`}
+      style={{ height: appSize >= 950 ? "60%" : "auto" }}
+    >
+      <div className='d-flex-row jc-center' style={{ flex: '1' }}>
+        <About appSize={appSize} />
+      </div>
+      <img
         src={backgroundimage}
-        alt='Background Image'
+        style={
+          appSize >= 950
+            ? { width: "50%", height: "100%", objectFit: "cover" }
+            : { width: "auto", height: "auto", objectFit: "cover" }
+        }
       />
+      {/* <div
+        className="b-cover d-flex-row ai-center jc-center"
+        style={{
+          width: "50%",
+          height: "100%",
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: "center",
+          background: `url(${backgroundimage})`,
+        }}
+      >
+      </div> */}
     </div>
-  )
+  );
 }
--- src/assets/components/Text/Text.jsx
+++ src/assets/components/Text/Text.jsx
@@ -1,14 +1,43 @@
+import redlogo from "../../images/redlogo.png";
+
 export default function Text({ appSize }) {
-  return(
-    <div className="p-48 d-flex-column jc-center">
-      <div className="pt-16 pb-16">
-        <p className="ff-title fs-32">G. Sathan</p>
+  return (
+    <div
+      className="p-32 d-flex-column jc-center"
+      style={
+        appSize >= 950
+          ? { maxWidth: "800px" }
+          : { flex: "auto", maxWidth: "800px", alignSelf: "center" }
+      }
+    >
+      <div className="pt-24 pb-24 d-flex-row">
+        <div className="pos-relative d-flex-row ai-center jc-center">
+          <p className="ff-title fs-32" style={{ zIndex: "1" }}>
+            G. Sathan
+          </p>
+          <img
+            src={redlogo}
+            className="pos-absolute"
+            style={{ width: "45px" }}
+          />
+        </div>
       </div>
       <div className="w-full">
-        <p className="w-full mb-5">J’ai débuté mon activité de tatouage il y a 5 ans après avoir été proche du milieu pendant de nombreuses années.</p>
-        <p className="w-full mb-5">Bien que n’ayant pas de formation de dessinateur mais une affinité marqué pour les arts plastiques, c’est instantanément que j’ai compris que ce métier était fait pour moi.</p>
-        <p className="w-full">Une fois débarrassé de tous les préjugés, c’est un métier qui se révèle très complet et au sommet, à mon sens en tout cas, de la collaboration entre le client et l’artiste.</p>
+        <p className="w-full mb-5">
+          J’ai débuté mon activité de tatouage il y a 5 ans après avoir été
+          proche du milieu pendant de nombreuses années.
+        </p>
+        <p className="w-full mb-5">
+          Bien que n’ayant pas de formation de dessinateur mais une affinité
+          marqué pour les arts plastiques, c’est instantanément que j’ai compris
+          que ce métier était fait pour moi.
+        </p>
+        <p className="w-full">
+          Une fois débarrassé de tous les préjugés, c’est un métier qui se
+          révèle très complet et au sommet, à mon sens en tout cas, de la
+          collaboration entre le client et l’artiste.
+        </p>
       </div>
     </div>
-  )
+  );
 }
