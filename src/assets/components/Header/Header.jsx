780f7af0a20f650d1709ccda9be5ac83db4fcd7f
Bluegnarl <dradarkx@gmail.com>
2023-04-27 00:09:49 +0200

    More animations for Header + About

--- src/assets/components/Header/Header.jsx
+++ src/assets/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import backgroundimage from "../../images/backgroundimage.png";
+import headerImage from "../../images/header.png";
 import About from "../About/About";
 import { useRef, useState, useEffect } from "react";
 
@@ -38,77 +38,94 @@ export default function Header({ appSize }) {
       <div className="d-flex-row jc-center" style={{ flex: "1" }}>
         <About appSize={appSize} visible={visible} />
       </div>
-
-        <img
-        src={backgroundimage}
+      <div
+        className="pos-relative"
         style={
           appSize < 180
             ? {
-              width: "auto",
-              height: visible ? '150px' : "0",
-              objectFit: "cover",
-              opacity: visible ? "1" : "0",
-              transition: "all .8s",
-            }
+                width: "100%",
+                height: visible ? "150px" : "0",
+                opacity: visible ? "1" : "0",
+                transition: "all .8s",
+              }
             : appSize < 250
             ? {
-              width: "auto",
-              height: visible ? '200px' : "0",
-              objectFit: "cover",
-              opacity: visible ? "1" : "0",
-              transition: "all .8s",
-            }
+                width: "100%",
+                height: visible ? "200px" : "0",
+                opacity: visible ? "1" : "0",
+                transition: "all .8s",
+              }
             : appSize < 300
             ? {
-              width: "auto",
-              height: visible ? '250px' : "0",
-              objectFit: "cover",
-              opacity: visible ? "1" : "0",
-              transition: "all .8s",
-            }
+                width: "100%",
+                height: visible ? "250px" : "0",
+                opacity: visible ? "1" : "0",
+                transition: "all .8s",
+              }
             : appSize < 350
             ? {
-              width: "auto",
-              height: visible ? '300px' : "0",
-              objectFit: "cover",
-              opacity: visible ? "1" : "0",
-              transition: "all .8s",
-            }
+                width: "100%",
+                height: visible ? "300px" : "0",
+                opacity: visible ? "1" : "0",
+                transition: "all .8s",
+              }
             : appSize < 391
             ? {
-              width: "auto",
-              height: visible ? '350px' : "0",
-              objectFit: "cover",
-              opacity: visible ? "1" : "0",
-              transition: "all .8s",
-            }
+                width: "100%",
+                height: visible ? "350px" : "0",
+                opacity: visible ? "1" : "0",
+                transition: "all .8s",
+              }
             : appSize < 450
             ? {
-                width: "auto",
-                height: visible ? '400px' : "0",
-                objectFit: "cover",
+                width: "100%",
+                height: "400px",
                 opacity: visible ? "1" : "0",
                 transition: "all .8s",
               }
             : appSize < 950
             ? {
                 width: "100%",
-                height: visible ? '500px' : "0",
-                objectFit: "cover",
-                objectPosition: "0 0",
+                height: visible ? "600px" : "0",
                 opacity: visible ? "1" : "0",
                 transition: "all .8s",
               }
             : {
                 width: visible ? "50%" : "70%",
-                height: "100%",
-                objectFit: "cover",
                 opacity: visible ? "1" : "0",
                 transition: "all .8s",
               }
+        }
+      >
+        <img
+          src={headerImage}
+          style={
+            appSize < 950
+              ? {
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover",
+                  objectPosition: "0 0",
+                }
+              : {
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover",
+                }
           }
         />
-
+        <div
+          className="pos-absolute"
+          style={{
+            top: "0",
+            right: "0",
+            bottom: "0",
+            left: "0",
+            background:
+              "linear-gradient(90deg, rgba(0, 0, 0, 0.95) 0%, rgba(0, 0, 0, 0) 18.2%)",
+          }}
+        ></div>
+      </div>
       {/* <div
         className="b-cover d-flex-row ai-center jc-center"
         style={{
